e31dae0b5d3b4aa7b1968892d7597556a50dda40
nonsec <iamsmokingone@gmail.com>
2017-06-03 20:44:45 -0400

    Add files via upload
    not functional at this point but some of the desired goals have been commented out.

---
+++ thermite.rs
@@ -0,0 +1,18 @@
+use std::io;
+use std::cmp::Ordering;
+use std::net::dns;
+
+fn main() {
+    println!("==============THERMITE *--*  HOST *--* ENUMERATOR=============");
+    // Input either a specific host or a range of hosts, by ip or name, thermite will handle finding ips in sequence until the count is met. Thermite will match an ip name to other similar names. If the input is not resolveable thermite will find the next closest ip or website.//
+    //URI resolver//
+    let uri = std::net.dns(std::io);
+//Thermite parses URI's and matches first the switch, which will set a counter bit for the switch, then the string and begins scanning//
+
+        println!(" !: {}", Starting scan); {
+            Ordering::First = (&uri); {
+    println!("scan completed");
+                break;
+            }
+        }
+    }
